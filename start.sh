9b4d89569bff09c4f74bb8d43cd77ed1e306bcd3
Kirikaze Chiyuki <me@chyk.ink>
2024-04-13 21:28:48 +0800

    [fix] Wayland 下鼠标指针不正确问题

--- start.sh
+++ start.sh
@@ -120,6 +120,7 @@ bwrap --new-session --cap-drop ALL --unshare-user-try --unshare-pid --unshare-cg
     --ro-bind-try "${HOME}/.icons" "${HOME}/.icons" \
     --ro-bind-try "${HOME}/.local/share/.icons" "${HOME}/.local/share/.icons" \
     --ro-bind-try "${XDG_CONFIG_HOME}/gtk-3.0" "${XDG_CONFIG_HOME}/gtk-3.0" \
+    --ro-bind-try "${XDG_CONFIG_HOME}/dconf" "${XDG_CONFIG_HOME}/dconf" \
     --ro-bind /etc/nsswitch.conf /etc/nsswitch.conf \
     --ro-bind /run/systemd/userdb/ /run/systemd/userdb/ \
     --setenv IBUS_USE_PORTAL 1 \
